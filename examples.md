df5c74b5e0cc6fd1483c45f727cef9deda072acc
Jeffrey Yang <jeffy@ifit.com>
2024-09-24 07:01:20 -0700

    chore: delete-user din SNS SQS testing

--- examples.md
+++ examples.md
@@ -1,2 +1,13 @@
+# Some AWS CLI Commands
 
-aws sqs send-message --endpoint-url http://localhost:4566 --region=us-east-1 --queue-url http://localhost:4566/000000000000/din-dev-delete-user --message-body "YO MOMMA"
+```sh
+# aws sqs send-message --endpoint-url http://localhost:4566 --region=us-east-1 --queue-url http://localhost:4566/000000000000/din-dev-delete-user --message-body "YO MOMMA"
+
+aws --endpoint-url=http://localhost:4566 --region=us-east-1 sqs list-queues
+
+aws --endpoint-url=http://localhost:4566 --region=us-east-1 sns list-subscriptions
+
+aws --endpoint-url=http://localhost:4566 --region=us-east-1 sns list-topics
+
+aws sns publish --endpoint-url=http://localhost:4566 --region=us-east-1 --topic-arn=arn:aws:sns:us-east-1:000000000000:user-service-local-onUserDelete --message '{"_id": "abc"}'
+```
--- localstack-create-resources.sh
+++ localstack-create-resources.sh
@@ -2,24 +2,24 @@
 
 # SQS Queue w/ DLQ
 
-echo "Create local SQS DLQ for local SQS Queue din-sample-queue-dlq"
+echo "Create local SQS DLQ for local SQS Queue din-dev-delete-user-dlq"
 aws \
   sqs create-queue \
-  --queue-name="din-sample-queue-dlq" \
+  --queue-name="din-dev-delete-user-dlq" \
   --endpoint-url="http://localhost:4566"
 
-echo "Create local SQS Queue din-sample-queue"
+echo "Create local SQS Queue din-dev-delete-user"
 aws \
   sqs create-queue \
-  --queue-name="din-sample-queue" \
-  --attributes='{"DelaySeconds":"0","RedrivePolicy":"{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:din-sample-queue-dlq\",\"maxReceiveCount\": \"3\"}"}' \
+  --queue-name="din-dev-delete-user" \
+  --attributes='{"DelaySeconds":"0","RedrivePolicy":"{\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:din-dev-delete-user-dlq\",\"maxReceiveCount\": \"3\"}"}' \
   --endpoint-url="http://localhost:4566"
 
 # SNS Topic w/ SQS Subscription
 
-echo "Create local SNS Topic din-sample-sns-topic"
+echo "Create local SNS Topic user-service-local-onUserDelete"
 din_sample_sns_topic_arn=$(aws sns create-topic \
-  --name "din-sample-sns-topic" \
+  --name "user-service-local-onUserDelete" \
   --endpoint-url "http://localhost:4566" \
   --query "TopicArn" \
   --output text)
@@ -29,7 +29,7 @@ if [ -n "$din_sample_sns_topic_arn" ]; then
   aws sns subscribe \
     --topic-arn "$din_sample_sns_topic_arn" \
     --protocol sqs \
-    --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:din-sample-queue" \
+    --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:din-dev-delete-user" \
     --endpoint-url "http://localhost:4566"
 else
   echo "TopicArn is empty. Unable to subscribe SQS to SNS topic. SNS topic creation may have failed."
